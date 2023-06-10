2c579a7ee91e307644748b3cdcc04b081924b0aa
Jared Kroke <jared.kroke@gmail.com>
2023-06-10 12:44:41 -0500

    Finished server routes and tested them.

--- server/routes/players.router.js
+++ server/routes/players.router.js
@@ -5,7 +5,7 @@ const router = express.Router();
 // This route is used to select the current player of the game.
 router.get('/:id', (req, res) => {
     const person = req.params.id;
-    const queryText = `SELECT "id", "name" FROM players WHERE "name" = $1;`;
+    const queryText = `SELECT * FROM players WHERE "name" = $1;`;
     pool.query(queryText, [person]).then(response => {
         res.send(response.rows);
     }).catch(error => {
@@ -20,6 +20,19 @@ router.post('/add', (req, res) => {
     const queryText = `INSERT INTO players ("name") 
     VALUES ($1);`;
     pool.query(queryText, [person.name]).then(response => {
+        res.sendStatus(201);
+    }).catch(error => {
+        console.log(error);
+        res.sendStatus(500);
+    });
+});
+
+// Route for score initialization to 0 when a new player is added to the game.
+router.post('/score', (req, res) => {
+    const newScore = req.body;
+    const queryText = `INSERT INTO scores ("wins", "player_id")
+    VALUES ('0', $1);`;
+    pool.query(queryText, [newScore.player_id]).then(response => {
         res.sendStatus(201);
     }).catch(error => {
         console.log(error);
